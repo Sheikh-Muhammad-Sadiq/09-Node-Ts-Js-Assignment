7697b6372176d57ab48e33d66b4f5a7eb339fc4b
Sheikh-Muhammad-Sadiq <162907621+Sheikh-Muhammad-Sadiq@users.noreply.github.com>
2024-03-17 03:11:48 -0700

    Add files via upload
    Q 09 Node.js of 45 Assignments

---
+++ Q9.js
@@ -0,0 +1,4 @@
+"use strict";
+// 9. Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that Message.
+let num = 1232;
+console.log(`My Favorite Number is ${num}`);
